130f00fe2bb747608127b1d1a36f75cbefcba298
438516 <70022856+438516@users.noreply.github.com>
2024-10-27 20:28:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,22 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+#十二、几个修改项
+#更改设备名为“N4505”
+sed -i "s/hostname='OpenWrt'/hostname='N4505'/g" package/base-files/files/bin/config_generate
+
+#修改默认主题
+sed -i 's/bootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
+
+#设置管理密码为空
+#sed -i 's/CYXluq4wUazHjmCDBCqXF/g' package/base-files/files/etc/shadow
+
+
+#设置TTYD登录无需密码
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+#设置管理密码为438516
+sed -i 's/root:$1$Tp0WlehU$7fTieRPHm4ie41xa22r9T/:19673:0:99999:7:::/g' package/base-files/files/etc/shadow
+
