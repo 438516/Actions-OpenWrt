0b908d5b3238adb3ec228df1b16436a484d2406c
438516 <70022856+438516@users.noreply.github.com>
2024-10-27 20:37:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,22 +13,31 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-
-#十二、几个修改项
-#更改设备名为“N4505”
+# 0-设置主机名
+# sed -i "s/hostname='OpenWrt'/hostname='RopWrt'/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='OpenWrt'/hostname='N4505'/g" package/base-files/files/bin/config_generate
 
-#修改默认主题
+# 1-设置默认主题
 sed -i 's/bootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
 
-#设置管理密码为空
-#sed -i 's/CYXluq4wUazHjmCDBCqXF/g' package/base-files/files/etc/shadow
+# 2-设置管理地址
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
+# 3-编译内核版本
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 
-#设置TTYD登录无需密码
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+# 4-设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-#设置管理密码为438516
-sed -i 's/root:$1$Tp0WlehU$7fTieRPHm4ie41xa22r9T/:19673:0:99999:7:::/g' package/base-files/files/etc/shadow
+# 5-修改时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
+# 6-添加固件日期
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+
+# 7-修正硬件信息
+sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
+
+# 8-增固件连接数
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
